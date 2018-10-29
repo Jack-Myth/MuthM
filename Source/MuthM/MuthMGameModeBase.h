46b9af3a9ed9065b0b03c67d2e4045df8e665196
Jack-Myth <wwwbkkk@126.com>
2018-10-29 18:18:31 +0800

    Init Push

---
+++ Source/MuthM/MuthMGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MuthMGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MUTHM_API AMuthMGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
