2d9388beef020d44325ba847fcb3a7ec949c3252
Jack-Myth <wwwbkkk@126.com>
2018-11-01 22:58:53 +0800

    Fix LoadIntFromData()

--- Source/MuthM/Public/MuthMTypeHelper.h
+++ Source/MuthM/Public/MuthMTypeHelper.h
@@ -13,10 +13,10 @@ class MUTHM_API MuthMTypeHelper
 public:
 	inline static uint32 LoadIntFromData(const uint8* pData)
 	{
-		uint32 tmp = *pData;
-		tmp += *(pData + 1) << 8;
-		tmp += *(pData + 2) << 16;
-		tmp += *(pData + 3) << 27;
+		uint32 tmp = *(pData + 3);
+		tmp = tmp << 8 + *(pData + 2);
+		tmp = tmp << 8 + *(pData + 1);
+		tmp = tmp << 8 + *pData;
 		return tmp;
 	}
 };
