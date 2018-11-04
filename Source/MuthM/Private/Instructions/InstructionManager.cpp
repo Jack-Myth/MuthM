24cd5b652bd59d512eaa1ba85fae9e9f4ab3de60
Jack-Myth <wwwbkkk@126.com>
2018-11-04 13:12:09 +0800

    Update emmx

---
+++ Source/MuthM/Private/Instructions/InstructionManager.cpp
@@ -0,0 +1,6 @@
+// // Copyright (C) 2018 JackMyth. All Rights Reserved.
+
+#include "InstructionManager.h"
+
+
+// Add default functionality here for any IInstructionManager functions that are not pure virtual.
---
+++ Source/MuthM/Public/Instructions/InstructionManager.h
@@ -0,0 +1,27 @@
+// // Copyright (C) 2018 JackMyth. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "InstructionManager.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UInstructionManager : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MUTHM_API IInstructionManager
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	
+	
+};
