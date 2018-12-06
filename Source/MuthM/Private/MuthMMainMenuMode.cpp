aca6c8a5d5b23c795acfb582292a32c70dd550d4
Jack-Myth <wwwbkkk@126.com>
2018-12-06 18:01:45 +0800

    2018.12.6 18:02

--- Source/MuthM/Private/MuthMMainMenuMode.cpp
+++ Source/MuthM/Private/MuthMMainMenuMode.cpp
@@ -2,12 +2,13 @@
 
 #include "MuthMMainMenuMode.h"
 #include "UIProvider.h"
+#include "WelcomeUIBase.h"
 
 void AMuthMMainMenuMode::LoadWelcome()
 {
-	//auto LogoAnimationClass = UUIProvider::Get()->GetWelcomeUI();
-	//auto* LogoAnimation = UUserWidget::CreateWidgetInstance(*GetWorld(), LogoAnimationClass, "LogoAnimation");
-	//LogoAnimation->AddToViewport();
+	auto WelcomeUIClass = UUIProvider::Get()->GetWelcomeUI();
+	auto* WelcomeUI = Cast<UWelcomeUIBase>(UUserWidget::CreateWidgetInstance(*GetWorld(), WelcomeUIClass, "WelcomeUI"));
+	WelcomeUI->AddToViewport();
 }
 
 void AMuthMMainMenuMode::BeginPlay()
